1f93d2bb57f5fe2d5db5fdfc01c8f65d103d3871
Matt Dooner <mattd@kainos.com>
2019-02-06 15:52:48 +0000

    initial commit

---
+++ common-security-config/src/main/java/uk/gov/defra/tracesx/TracesXSpringBootCommonSecurity/TracesXSpringBootCommonSecurityApplication.java
@@ -0,0 +1,14 @@
+package uk.gov.defra.tracesx.TracesXSpringBootCommonSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracesXSpringBootCommonSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracesXSpringBootCommonSecurityApplication.class, args);
+	}
+
+}
+
