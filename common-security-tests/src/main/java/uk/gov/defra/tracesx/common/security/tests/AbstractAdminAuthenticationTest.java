ec29acf3f9d5b5b5385466a82fb49b69e53999f2
Matt Dooner <mattd@kainos.com>
2019-02-25 14:05:31 +0000

    Added an abstract test class for testing authentication on the admin urls

---
+++ common-security-tests/src/main/java/uk/gov/defra/tracesx/common/security/tests/AbstractAdminAuthenticationTest.java
@@ -0,0 +1,34 @@
+package uk.gov.defra.tracesx.common.security.tests;
+
+import static io.restassured.RestAssured.given;
+import static uk.gov.defra.tracesx.common.security.tests.Constants.X_AUTH_BASIC;
+import static uk.gov.defra.tracesx.common.security.tests.Constants.X_AUTH_BASIC_VALUE;
+
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+public abstract class AbstractAdminAuthenticationTest {
+
+  protected abstract String getAdminUrl();
+
+  @Test
+  public void callAdmin_withoutBasicAuth_successfully() {
+    given()
+        .contentType(ContentType.JSON)
+        .when()
+        .get(getAdminUrl())
+        .then()
+        .statusCode(200);
+  }
+
+  @Test
+  public void callAdmin_withLegacyAuthHeader_headerIgnoredBackwardsCompatible() {
+    given()
+        .contentType(ContentType.JSON)
+        .header(X_AUTH_BASIC, X_AUTH_BASIC_VALUE)
+        .when()
+        .get(getAdminUrl())
+        .then()
+        .statusCode(200);
+  }
+}
