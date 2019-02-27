f2d0b304df9847870fde52ca0aad77c79a39fa0a
Matt Dooner <mattd@kainos.com>
2019-02-27 16:19:11 +0000

    removed some offensive javadoc and turned it into a readme

--- common-security-tests/src/main/java/uk/gov/defra/tracesx/common/security/tests/AbstractApiAuthenticationTest.java
+++ common-security-tests/src/main/java/uk/gov/defra/tracesx/common/security/tests/AbstractApiAuthenticationTest.java
@@ -1,43 +1,20 @@
 package uk.gov.defra.tracesx.common.security.tests;
 
 import static io.restassured.RestAssured.given;
-import static uk.gov.defra.tracesx.common.security.tests.jwt.JwtConstants.*;
+import static uk.gov.defra.tracesx.common.security.tests.jwt.JwtConstants.AUD;
+import static uk.gov.defra.tracesx.common.security.tests.jwt.JwtConstants.BEARER;
+import static uk.gov.defra.tracesx.common.security.tests.jwt.JwtConstants.ISS;
+import static uk.gov.defra.tracesx.common.security.tests.jwt.SelfSignedTokenClient.TokenType.AD;
+import static uk.gov.defra.tracesx.common.security.tests.jwt.SelfSignedTokenClient.TokenType.B2C;
 
 import io.restassured.http.ContentType;
-import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.FromDataPoints;
 import org.junit.experimental.theories.Theory;
 import uk.gov.defra.tracesx.common.security.tests.jwt.SelfSignedTokenClient;
 import uk.gov.defra.tracesx.common.security.tests.jwt.SelfSignedTokenClient.TokenType;
-import static uk.gov.defra.tracesx.common.security.tests.jwt.SelfSignedTokenClient.TokenType.*;
 
-/**
- * <p>To use: extend this class and declare a static @DataPoints field named {@link #DATA_POINTS_NAME}
- * that is an array of lambda which consume a {@link RequestSpecification} and produce a {@link
- * Response}. The {@link RequestSpecification} provided to the lambda will already contain the
- * security headers required for the test. The lambda should provide any further parameters and call
- * the appropriate method to complete the request e.g. {@link RequestSpecification#get()} or {@link
- * RequestSpecification#post()}. There should be a lambda for every API exposed by the service so
- * that security of each endpoint can be verified.</p>
- *
- * <p>The subclass needs to be annotated <pre>@RunWith(Theories.class)</pre>.</p>
- *
- * <p>Example: </p>
- * <code>
- *   \@DataPoints(DATA_POINTS_NAME)
- *   public ApiMethod[] getApiMethods() {
- *     return new ApiMethod[]{
- *         spec -> spec.get(helper.getAllCountries()),
- *         spec -> spec.get(helper.getNonUKCountries()),
- *         spec -> spec.get(helper.getCountryById("MY")),
- *         spec -> spec.post(helper.postCountries()),
- *         spec -> spec.delete(helper.deleteCountries("AB"))
- *     };
- *   }
- * </code>
- */
 @SuppressWarnings("unused")
 public abstract class AbstractApiAuthenticationTest {
 
