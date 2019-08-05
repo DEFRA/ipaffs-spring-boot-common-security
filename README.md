e867e2b09511f54b1c4b60d7fe08d00e69399b17
Lynn Eagleton (KAINOS) <lynn.eagelton@defra.onmicrosoft.com>
2019-08-05 09:05:43 +0100

    IMTA-5587 Create private economic operator
    + Updated the security library so Economic Operators service can be updated to use this latest version.

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 ## common-security-config
 
 This module should be included as a dependency for Spring projects that use JWT Tokens and Permissions for
-security. Included is Spring configuration classes, Servlet Filters used to enforce security policies,
+security. Included are Spring configuration classes, Servlet Filters used to enforce security policies,
 and a client for calling the permissions service.
 
 ## common-security-test
 
 This module should be included as a test dependency of the integration tests for any service using
-`common-security-config`. It include two abstract test classes that must be extended to verify security
+`common-security-config`. It includes two abstract test classes that must be extended to verify security
 is working correctly in the service.
--- common-security-config/src/test/java/uk/gov/defra/tracesx/common/permissions/PermissionsClientTest.java
+++ common-security-config/src/test/java/uk/gov/defra/tracesx/common/permissions/PermissionsClientTest.java
@@ -1,7 +1,5 @@
 package uk.gov.defra.tracesx.common.permissions;
 
-import static java.lang.Boolean.TRUE;
-import static java.util.Collections.EMPTY_LIST;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -10,7 +8,6 @@ import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpStatus.OK;
 
-import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,6 +19,9 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PermissionsClientTest {
@@ -36,25 +36,26 @@ public class PermissionsClientTest {
   private static final String PERMISSIONS_URL = "permissionsUrl";
   private static final String PERMISSIONS_USER = "permissionsUser";
   private static final String PERMISSIONS_PASSWORD = "permissionsPassword";
-  private static final String SECURITY_TOKEN_FEATURE_SWITCH = "securityTokenFeatureSwitch";
   private final List<String> perms = singletonList(READ);
 
-  @Mock private RestTemplate restTemplate;
-  @InjectMocks private PermissionsClient permissionsService;
+  @Mock
+  private RestTemplate restTemplate;
+  @InjectMocks
+  private PermissionsClient permissionsService;
 
   @Before
   public void setup() {
     ReflectionTestUtils.setField(permissionsService, PERMISSIONS_USER, USER);
     ReflectionTestUtils.setField(permissionsService, PERMISSIONS_PASSWORD, PASSWORD);
-    ReflectionTestUtils.setField(permissionsService, SECURITY_TOKEN_FEATURE_SWITCH, TRUE);
     ReflectionTestUtils.setField(permissionsService, PERMISSIONS_URL, URL);
 
     final ResponseEntity<List<String>> responseEntity = createResponseEntity();
     when(restTemplate.exchange(
-            any(),
-            eq(GET),
-            any(HttpEntity.class),
-            eq(new ParameterizedTypeReference<List<String>>() {})))
+        any(),
+        eq(GET),
+        any(HttpEntity.class),
+        eq(new ParameterizedTypeReference<List<String>>() {
+        })))
         .thenReturn(responseEntity);
   }
 
@@ -71,11 +72,12 @@ public class PermissionsClientTest {
   public void testWhenPermissionsCalledWithInvalidRoleThenReturnEmptyList() {
 
     when(restTemplate.exchange(
-            any(),
-            eq(GET),
-            any(HttpEntity.class),
-            eq(new ParameterizedTypeReference<List<String>>() {})))
-        .thenReturn(new ResponseEntity<>(EMPTY_LIST, OK));
+        any(),
+        eq(GET),
+        any(HttpEntity.class),
+        eq(new ParameterizedTypeReference<List<String>>() {
+        })))
+        .thenReturn(new ResponseEntity<>(Collections.emptyList(), OK));
 
     final List<String> permissionsList = permissionsService.permissionsList(INVALID_ROLE, TOKEN);
 
